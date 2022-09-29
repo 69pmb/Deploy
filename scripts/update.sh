04d2500859194543923ea8e0fc460b5ff191fdcf
69pmb <pmbroca@gmail.com>
2022-09-29 22:30:33 +0200

    fix: replace script in update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 function update() {
     if [ -L "$1" ] ; then rm $1.sh ; fi
-    wget -q --no-cache https://raw.githubusercontent.com/69pmb/Deploy/main/scripts/$1.sh
+    wget -q --no-cache https://raw.githubusercontent.com/69pmb/Deploy/main/scripts/$1.sh -O $1.sh
     chmod +x $1.sh
     if [ -L "/usr/bin/$1" ] ; then sudo rm /usr/bin/$1 ; fi
     sudo ln -s ~/workspace/$1.sh /usr/bin/$1

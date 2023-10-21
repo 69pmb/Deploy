2a9c8d27d31c55b0ac96e591fe779bdf4982e9a1
69pmb <pmbroca@gmail.com>
2023-10-22 00:26:33 +0200

    Feat/pnpm (#28)
    * feat: pnpm
    
    * feat: angular node version

--- docker/ci-webhook/deploy.sh
+++ docker/ci-webhook/deploy.sh
@@ -4,17 +4,18 @@ set -eo pipefail
 ## Script's arguments
 project=$1
 branch=$2
-args=$3
-confFile=$4
-directory=$5
-port=$6
+sha=$3
+deployBranch=$4
+args=$5
+confFile=$6
+directory=$7
+port=$8
 
 ## Constants
-buildBranch="main"
 deployRepo="https://raw.githubusercontent.com/69pmb/Deploy"
-deployFile="$deployRepo/$buildBranch/deploy-properties.json"
-ngUrl="$deployRepo/$buildBranch/docker/ng-build/Dockerfile"
-jUrl="$deployRepo/$buildBranch/docker/jbuild/Dockerfile"
+deployFile="$deployRepo/$deployBranch/deploy-properties.json"
+ngUrl="$deployRepo/$deployBranch/docker/ng-build/Dockerfile"
+jUrl="$deployRepo/$deployBranch/docker/jbuild/Dockerfile"
 ngDockerfile=$(curl -s $ngUrl)
 keySeparator="@"
 valueSeparator=","
@@ -24,8 +25,9 @@ declare -A NodeMap
 NodeMap[10]=14.18.2-alpine3.12
 NodeMap[11]=14.18.2-alpine3.12
 NodeMap[12]=14.18.2-alpine3.12
-NodeMap[13]=16.12.0-alpine3.12
-NodeMap[14]=16.12.0-alpine3.12
+NodeMap[13]=16.20.0-alpine3.17
+NodeMap[14]=16.20.0-alpine3.17
+NodeMap[15]=16.20.0-alpine3.17
 
 # Find docker file argument value
 function getArgVersion() {
@@ -50,28 +52,39 @@ function getAngularVersion() {
 # Test if project is an Angular one
 function isAngularProject() {
     local isNg=$(curl -s -I "https://raw.githubusercontent.com/$1/$2/$3/package.json" | grep -E "^HTTP" | awk -F " " '{print $2}')
-    if [[ $isNg -eq 200 ]]
-        then echo 1
-        else echo 0
+    if [[ $isNg -eq 200 ]]; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+# Test if project is an pnpm one
+function isPnpmProject() {
+    local isPnpm=$(curl -s -I "https://raw.githubusercontent.com/$1/$2/$3/pnpm-lock.yaml" | grep -E "^HTTP" | awk -F " " '{print $2}')
+    if [[ $isPnpm -eq 200 ]]; then
+        echo "pnpm"
+    else
+        echo "npm"
     fi
 }
 
 # Find port in property file
 function getPort() {
-  apps=$(echo $1 | jq '.apps[] | .name |= ascii_downcase')
-  app_port=$(echo $apps | jq 'select(.name == '\"$2\"')' | grep port | cut -d: -f 2 | sed 's/,//g')
-  if [[ -z $app_port ]]
-  then echo 8080
-  else echo $app_port
-  fi
+    apps=$(echo $1 | jq '.apps[] | .name |= ascii_downcase')
+    app_port=$(echo $apps | jq 'select(.name == '\"$2\"')' | grep port | cut -d: -f 2 | sed 's/,//g')
+    if [[ -z $app_port ]]; then
+        echo 8080
+    else
+        echo $app_port
+    fi
 }
 
 # Replace placeholder value with given args in specific file
 function replaceArgs() {
     file=$1
-    IFS=$keySeparator read -a args <<< $2
-    for i in "${args[@]}"
-     do
+    IFS=$keySeparator read -a args <<<$2
+    for i in "${args[@]}"; do
         key=$(echo $i | cut -d $valueSeparator -f 1)
         value=$(echo $i | cut -d $valueSeparator -f 2)
         file=$(echo "$file" | sed "s/{{$key}}/$value/")
@@ -80,57 +93,55 @@ function replaceArgs() {
 }
 
 # Validate & process arguments
-if [[ -z $project ]]
-then
-    echo "Project is required";
-    exit 1;
+if [[ -z $project ]]; then
+    echo "Project is required"
+    exit 1
 fi
-if [[ -z $branch ]]
-then
+if [[ -z $branch ]]; then
     branch="main"
-    echo "Branch not provided, default is '$branch'";
+    echo "Branch not provided, default is '$branch'"
+fi
+if [[ -z $deployBranch ]]; then
+    deployBranch="main"
+    echo "Deploy branch not provided, default is '$deployBranch'"
 fi
 clean_branch=$(echo $branch | sed -e "s/\//-/g")
-if [[ -z $directory ]]
-then
-    if [[ $(echo $project | grep '/' | wc -l) -eq 1 ]]
-    then
+if [[ -z $directory ]]; then
+    if [[ $(echo $project | grep '/' | wc -l) -eq 1 ]]; then
         directory=$(echo $project | cut -d "/" -f1)
         project=$(echo $project | cut -d "/" -f2)
     else
-        directory="69pmb"
-        echo "Directory not provided, default is '$directory'";
+        directory="690pmb"
+        echo "Directory not provided, default is '$directory'"
     fi
 fi
 
 apps=$(curl -s $deployFile)
 names=$(echo $apps | jq .apps[].name | sed 's/"//g')
-if [[ ! $(echo $names | grep -wi $project | wc -l) -eq 1 ]]
-then
+if [[ ! $(echo $names | grep -wi $project | wc -l) -eq 1 ]]; then
     echo "Specified project '$project' is not recognized"
-    exit 1;
+    exit 1
 fi
 isBrExist=$(isBranchExist "$directory" "$project" "$branch")
-if [[ $isBrExist -eq 0 ]]
-then
+if [[ $isBrExist -eq 0 ]]; then
     echo "Specified branch '$branch' does not exist on project '$project'"
-    exit 1;
+    exit 1
 fi
 
 # Build docker image
-cmdBuild="docker build --build-arg GITHUB_DIR=$directory --build-arg GITHUB_PROJECT=$project --build-arg GITHUB_HASH=$branch --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" "
+cmdBuild="docker build --build-arg GITHUB_DIR=$directory --build-arg GITHUB_PROJECT=$project --build-arg GITHUB_HASH=$branch --build-arg GITHUB_SHA=$sha --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" "
 image=${project,,}.$clean_branch
 
 isAngular=$(isAngularProject $directory $project $branch)
-if [[ $isAngular -eq 1 ]]
-then
+if [[ $isAngular -eq 1 ]]; then
     angularVersion=$(getAngularVersion $directory $project $branch)
     echo "Angular version detected: $angularVersion"
-   
+
     node=${NodeMap[$angularVersion]}
 
     nginx_version=$(getArgVersion "ng_nginx")
-    cmdBuild+=" --build-arg NODE_VERSION=$node --build-arg NG_NGINX_VERSION=$nginx_version -t $image $ngUrl"
+    isPnpm=$(isPnpmProject $directory $project $branch)
+    cmdBuild+=" --build-arg NODE_VERSION=$node --build-arg NG_NGINX_VERSION=$nginx_version --build-arg PNPM=$isPnpm -t $image $ngUrl"
 else
     cmdBuild+=" -t $image $jUrl"
 fi
@@ -141,11 +152,9 @@ eval "$cmdBuild"
 # Run the docker image
 name=$(echo $image | cut -d : -f 1 | cut -d / -f 2 | cut -d . -f 1)
 compose=$(curl -s https://raw.githubusercontent.com/$directory/$project/$branch/docker-compose.yml)
-if [[ $(echo $compose  | grep 404 | wc -l) -eq 1 ]]
-then
+if [[ $(echo $compose | grep 404 | wc -l) -eq 1 ]]; then
     # no docker compose file, using regular "docker run" command
-    if [[ -z $port ]]
-    then
+    if [[ -z $port ]]; then
         port=$(getPort "$apps" $name)
     fi
     cmdRun="docker run --name $name --restart unless-stopped -d -p $port:8080 -t $image"
@@ -153,13 +162,12 @@ then
     docker ps -qaf "name=$name" | xargs -r docker rm -f
     docker ps -qaf "publish=$port" | xargs -r docker rm -f
     eval "$cmdRun"
-    if [[ ! -z $confFile ]]
-    then
+    if [[ ! -z $confFile ]]; then
         docker cp $name:$confFile .
         fileName=$(echo $confFile | rev | cut -d "/" -f1 | rev)
         conf=$(cat $fileName)
         conf=$(replaceArgs "$conf" "$args")
-        echo "$conf" > $fileName
+        echo "$conf" >$fileName
         docker cp $fileName $name:$(echo $confFile | sed "s/$fileName//g")
     fi
 else
@@ -167,6 +175,6 @@ else
     compose=$(replaceArgs "$compose" "$args")
     compose=$(echo "$compose" | sed "s/image: $name[a-z\.]*/image: $image/")
     docker ps -aqf 'label=com.docker.compose.project=$project' | xargs -r docker rm -f
-    echo "$compose" > docker-compose.yml
+    echo "$compose" >docker-compose.yml
     env COMPOSE_PROJECT_NAME=$project docker compose up -d
 fi
